b27c175a90146b3df01c11ed5902190e023128f8
Guilherme <guilhermegomes09082003@gmail.com>
2022-05-10 08:40:29 -0300

    First commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,15 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+
+        Gato gato = new Gato();
+        System.out.println(gato);
+
+        /*int a = 1000;
+        int b = 7;
+        System.out.println("Hello word" + (a-b));*/
+    }
+}
